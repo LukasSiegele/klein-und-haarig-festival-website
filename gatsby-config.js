0cc9a363a559ae75e7f05d551c848b1fa47e2948
Lukas Siegele <lukas.siegele@gmx.de>
2021-01-27 22:56:15 +0100

    Title Template

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Klein und Haarig Festival`,
-    titleTemplate: "%s · The Real Hero",
+    titleTemplate: "%s · Klein und Haarig Festival",
     description: `Ein kleines Festival in Bad Wildbad vom 9. bis 11. Juli 2021`,
     author: `Bunte Platte e.V.`,
     url: `https://www.kleinundhaarig.de`,
