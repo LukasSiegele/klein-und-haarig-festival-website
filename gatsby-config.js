85618cc700b0f3e2e945617993948cb72431bfc1
Lukas Siegele <lukas.siegele@gmx.de>
2021-01-27 22:27:26 +0100

    lsdk

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `Ein kleines Festival in Bad Wildbad vom 9. bis 1. Juli 2021`,
     author: `Bunte Platte e.V.`,
     url: `https://www.kleinundhaarig.de`,
-    image: `/images/kuh-thumbnail.jpg`,
+    image: `/images/kuhthumbnail.jpg`,
   },
   plugins: [
     {
