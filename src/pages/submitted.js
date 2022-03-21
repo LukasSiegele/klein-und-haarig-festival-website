f7512f0ac60a435921068b141c16c44e5e8ff632
Lukas Siegele <lukas.siegele@gmx.de>
2022-03-21 18:28:00 +0100

    Submitted Page angefangen

--- src/pages/submitted.js
+++ src/pages/submitted.js
@@ -4,68 +4,63 @@ import styled from "styled-components"
 import FormButton from "../components/buttons/FormButton"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
-import ShopTitle from "../components/shopping/ShopTitle"
-import SubmittedImage from "../../static/images/Submitted.jpg"
+import LogoSVG from "../../static/images/LogoSideNav.svg"
+import BackgroundTexture from "../../static/images/BackgroundTexture.png"
+
+import { Headline, SubheaderSmall, Note } from "../components/styles/TextStyles"
 
 export default function Submitted() {
   return (
     <Layout>
       <SEO title="Submitted" />
       <Container>
-        <ShopTitle
-          info="Wouhuuuu!"
-          title="Wir haben deine Reservierung erhalten"
-        />
         <Wrapper>
-          <Section>
-            <Info>
-              Spätestens in ein paar Minuten solltest du eine Bestätigungsmail
-              von uns erhalten. Bitte checke auch deinen Spam-Ordner! Wenn du
-              keine E-Mail bekommen hast, melde dich bitte via
-              ticket@kleinundhaarig.de bei uns.
-            </Info>
-            <br />
-            <Info>
-              Wir freuen uns auf dich! <br />
-              Dein KuH Team
-            </Info>
-          </Section>
-          <ButtonSection>
+          <Left>
             <Link to="/">
-              <FormButton label="Zurück zur Homepage"></FormButton>
+              <Logo />
             </Link>
-          </ButtonSection>
+            <ButtonSection>
+              <Link to="/">
+                <FormButton label="Zurück zur Homepage"></FormButton>
+              </Link>
+            </ButtonSection>
+          </Left>
+          <Right></Right>
         </Wrapper>
       </Container>
     </Layout>
   )
 }
 
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr;
-  justify-items: center;
-  background-image: url(${SubmittedImage});
-  background-position: 50%;
-  background-size: cover;
-  border: solid black;
-  border-width: 40px 40px 0px 40px;
-  /* text-align: center; */
-  padding: 10px 20px 500px 20px;
-`
+const Container = styled.div``
 
 const Wrapper = styled.div`
-  max-width: 500px;
+  padding: 40px;
+  height: 900px;
+  background-image: url(${BackgroundTexture});
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+const Left = styled.div``
+const Right = styled.div``
+
+// KuH Logo in TopNav
+const Logo = styled.div`
+  width: 84px;
+  height: 79px;
+  margin-right: 40px;
+  background-image: url(${LogoSVG});
+  background-size: cover;
+  align-self: center;
+  @media (max-width: 800px) {
+    margin-right: 24px;
+  }
+  :hover {
+    cursor: pointer;
+  }
 `
 
-const Section = styled.div`
-  margin-bottom: 60px;
-`
-
-const Info = styled.h4`
-  margin-top: 10px;
-  color: white;
-`
+const Header = styled(Headline)``
 
 const ButtonSection = styled.div`
   padding: 0;
