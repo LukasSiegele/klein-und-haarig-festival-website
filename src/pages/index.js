ea31a9ce4a2ed57727f5957cec45fbb2835cf6b2
Lukas Siegele <lukas.siegele@gmx.de>
2022-12-02 13:07:49 +0100

    Es funktioniert

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import ReactDOM from "react-dom"
 import styled from "styled-components"
 import Hero from "../components/sections/Hero"
 import SideNavigation from "../components/navigation/SideNavigation"
