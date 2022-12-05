9bee005a460261c5a13c0ac932104fe587aaef25
Lukas Siegele <lukas.siegele@gmx.de>
2022-12-05 19:18:01 +0100

    no dom

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import ReactDOM from "react-dom"
 import styled from "styled-components"
 import Hero from "../components/sections/Hero"
 import SideNavigation from "../components/navigation/SideNavigation"
