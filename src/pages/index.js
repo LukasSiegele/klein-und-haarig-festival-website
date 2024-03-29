8315bc3e62b1f7dc06f950106543925dffb18507
Francesco Saccone <francescosaccone95@live.it>
2024-03-29 17:42:28 +0100

    WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -107,6 +107,21 @@ export default function IndexPage() {
   )
 }
 
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: {language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
+
 const ArtistText = styled(TextSmall)`
   position: absolute;
   mix-blend-mode: difference;
